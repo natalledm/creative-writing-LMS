a8917da563e59eb6799381cbf5855598d42295f7
Natalle <natallemoura7@gmail.com>
2022-04-28 14:36:26 +0200

    Add prettier, firebase, react router libraries

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+// import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React router</h1>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes> */}
+    </div>
+  );
+}

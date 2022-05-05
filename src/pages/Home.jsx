1751bf982e23d903124674d87960b84e209a0044
Natalle <natallemoura7@gmail.com>
2022-05-05 23:24:04 +0200

    Home page created and added images assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,32 @@
+import { Link } from "react-router-dom";
+import bgImg from "../assets/images/home-aaron-burden-unsplash.png";
+import "../styles/pages/home-page.css";
+
 export default function Home() {
-  return <div>Home here</div>;
+  return (
+    <div className="homepage">
+      <header
+        className="header-container"
+        style={{ backgroundImage: `url(${bgImg})` }}
+      >
+        <h1 className="header-title">Creative Writing</h1>
+        <h2 className="header-subtitle">Learning Management System</h2>
+      </header>
+      <section className="home-content">
+        <div className="content-container">
+          <p>Learn everything you want about Creative Writing with us!</p>
+          <p>
+            We offer courses about <strong>fiction</strong>,
+            <strong> non-fiction</strong>,<strong> poetry</strong>, and
+            <strong> copywriting</strong>. Each course has specialties and we
+            are always improving to bring you the best experience.
+          </p>
+          <p>Enroll now for free!</p>
+          <span className="main-button home-signup-button">
+            <Link to={"/singup"}>Sign up</Link>
+          </span>
+        </div>
+      </section>
+    </div>
+  );
 }

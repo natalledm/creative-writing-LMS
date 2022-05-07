925e41f9c7ac69948e0e1baa2f97e01adcfce6c4
Natalle <natallemoura7@gmail.com>
2022-05-07 22:53:05 +0200

    Update userInfo to have enrolled courses information

--- src/pages/DashboardStudent.jsx
+++ src/pages/DashboardStudent.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
 import { getCollection } from "../scripts/fireStoreDB";
+import { useUserId } from "../state/UserIdContext";
+import CourseCard from "../components/CourseCard";
+import { editDocument } from "../scripts/fireStoreDB";
 import "../styles/pages/dashboard-page.css";
 import "../styles/logged-content-layout.css";
 
 export default function DashboardStudent() {
+  const { userId, userInfo } = useUserId();
+
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
     async function loadData(path) {
       const data = await getCollection(path);
-      console.log(data);
       setCourses(data);
     }
     loadData("courses");
   }, []);
+
+  async function onEnroll(event, course) {
+    event.preventDefault();
+    // update local userInfo
+    userInfo.courses.push(course.id);
+    // update on db
+    await editDocument("users", userInfo, userId);
+  }
 
   return (
     <div className="logged-in-body">
@@ -21,7 +33,11 @@ export default function DashboardStudent() {
       <h3>Courses</h3>
       <ul>
         {courses.map((course) => (
-          <li key={course.id}>{course.id}</li>
+          <div>
+            <CourseCard key={course.id} course={course} showEnroll={true} />
+
+            <button onClick={(event) => onEnroll(event, course)}>Enroll</button>
+          </div>
         ))}
       </ul>
     </div>
